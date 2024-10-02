e1d40bf51be92dedc0369ceb8d34d7844617b8a3
Memo Cachú <gcachu.o@gmail.com>
2024-10-02 17:53:39 -0600

    Show access table

--- src/screens/AccessLog.tsx
+++ src/screens/AccessLog.tsx
@@ -1,15 +1,57 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
 import Drawer from "../components/Drawer";
 import { getVisits } from "../services/firebase/getVisits";
+import {Avatar, List, ListItem, ListItemAvatar, ListItemButton, ListItemText} from "@mui/material";
+
+interface Visit {
+    uid: string;
+    type: string;
+    guestName: string;
+    creatorUid: string;
+    createdTime: number;
+}
 
 function AccessLogScreen() {
-  useEffect(() => {
-    getVisits().then();
-  });
+  const [visits, setVisits] = useState([]);
 
+  useEffect(() => {
+    getVisits().then((result) => {
+      const visitList = result!.map((visit) => ({
+        uid: visit.uid,
+          type: visit.type,
+        guestName: visit.guestName,
+        creatorUid: visit.creatorUid,
+        createdTime: visit.createdTime,
+      }));
+      // @ts-ignore
+      setVisits(visitList);
+    })
+        .catch((error: any) => {
+          console.log("Error fetching user list:", error);
+        });
+  },[]);
+
+    console.log(visits);
   return (
     <div>
       <Drawer title={"Autorizaciones"} />
+      <List>
+        {visits &&
+            visits.sort((a: Visit,b: Visit)=>b.createdTime-a.createdTime).map(
+                (item: Visit) => (
+                    <ListItem key={item.uid}>
+                      <ListItemButton>
+                        <ListItemText
+                            primary={item.guestName}
+                            secondary={
+                              `${new Date(item.createdTime)} - ${item.creatorUid}`
+                            }
+                        />
+                      </ListItemButton>
+                    </ListItem>
+                )
+            )}
+      </List>
     </div>
   );
 }
